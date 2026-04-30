f0d71f21bfd6693c4d6b47eb30a7b4503743339b
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-04-30 15:00:39 -0500

    Create DishStack.java

---
+++ app/src/test/java/org/example/DishStack.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class DishStack {
+
+    private Dish[] stack;
+    private int top;
+    private int capacity;
+
+    public DishStack(int capacity) {
+        this.capacity = capacity;
+        this.stack = new Dish[capacity];
+        this.top = -1;
+    }
+
+    // push
+    public void push(Dish dish) {
+        if (top == capacity - 1) {
+            System.out.println("Stack is full");
+            return;
+        }
+        stack[++top] = dish;
+    }
+
+    // pop
+    public Dish pop() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+        return stack[top--];
+    }
+
+    // peek
+    public Dish peek() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+        return stack[top];
+    }
+
+    // size
+    public int size() {
+        return top + 1;
+    }
+}
